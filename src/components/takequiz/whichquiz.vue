6302aeb33af491cd115cd03275220cf6fc58551c
unknown <ahumphries84@outlook.com>
2020-08-02 20:44:45 -0500

    Need to fix undefined pop() issue in whichquiz

--- src/components/takequiz/whichquiz.vue
+++ src/components/takequiz/whichquiz.vue
@@ -2,7 +2,7 @@
   <v-container fill-height>
     <v-card :width="isMobile()?'100%':'50%'" class="mx-auto" v-if="dataFetched">
       <v-card-title>
-        <p>Question {{wQu + 1}}/{{allQuizzes[wQ].quiz.length}}: {{allQuizzes[wQ].quiz[0].question}}</p>
+        <p>Question {{wQu + 1}}/{{quizObj.quiz.length}}: {{quizObj.quiz[0].question}}</p>
       </v-card-title>
       <v-card-subtitle>
         <v-text-field v-model="scoreCard.userName">
@@ -13,20 +13,19 @@
       </v-card-subtitle>
       <v-card-text>
         <v-list-item
-          v-for="(quiz, ind) in allQuizzes[wQ].quiz[wQu].answers"
-          :class="currentAnswer === quiz.answered?'light-green':''"
-          :dark="currentAnswer === quiz.answered"
+          v-for="(answer, ind) in quizObj.quiz[wQu].answers"
+          :class="currentAnswer === answer?'light-green':''"
+          :dark="currentAnswer === answer"
           :style="{'border-radius':'50px'}"
           :key="ind"
-          @click="saveAnswer(quiz.answered)"
-        >{{String.fromCharCode('A'.charCodeAt(0)+ind)}}: {{quiz.answer}}</v-list-item>
+          @click="saveAnswer(answer)"
+        >{{String.fromCharCode('A'.charCodeAt(0)+ind)}}: {{answer.answer}}</v-list-item>
       </v-card-text>
       <v-card-actions>
         <v-btn v-show="wQu <= 0" color="primary" :to="{name:'takequiz'}">View Quizzes</v-btn>
         <v-btn v-show="wQu > 0" color="primary" @click="removeAnswer"><</v-btn>
         <v-btn
           color="primary"
-          :disabled="noSelectedAnswer || wQu >= allQuizzes[wQ].quiz.length + lr"
           @click="nextQuestion"
         >{{nextQuestionButtonText}}</v-btn>
       </v-card-actions>
@@ -34,7 +33,7 @@
     <EndScreen
       v-if="gameOver"
       @viewAnswers="viewAnswers"
-      :allQuizzes="allQuizzes"
+      :quizObj="quizObj"
       :scoreCard="scoreCard"
       :wQ="wQ"
     />
@@ -51,14 +50,14 @@ export default {
   },
   data() {
     return {
-      allQuizzes: [],
       currentAnswer: "",
       dataFetched: false,
       gameOver: false,
-      //used when to check the allQuizzes length -1
+      //used when to check the quizObj length -1
       lr: 1,
       nextQuestionButtonText: ">",
       noSelectedAnswer: true,
+      quizObj: [],
       scoreCard:{
         points:[],
         answerToChoose:[],
@@ -76,23 +75,15 @@ export default {
   },
   mounted() {
     this.viewQuiz();
-    
   },
   mixins: [isMobile],
   methods: {
     async viewQuiz() {
-      // const response = await this.$fetchData("GET", "/viewquiz/1", {
-      //   userName: this.$store.state.userName,
-      // });
-      console.log(this.$router.currentRoute)
-      const response = await this.$fetchData("GET", `/viewquiz/?name=`);
-      this.allQuizzes = response;
+      const response = await this.$fetchData("GET", `/viewquiz/?name=${this.$router.currentRoute.params.quizName}`);
+      this.quizObj = response[0];
       this.dataFetched = true;
-
-      this.allQuizzes.forEach(ele => {
-      this.scoreCard.quizName = ele.quizName
-    })
-    console.log(this.allQuizzes)
+      console.log(this.quizObj)
+      this.scoreCard.quizName = this.quizObj.quizName
     },
     nextQuestion() {
       this.nQ++;
@@ -102,7 +93,7 @@ export default {
         return acc;
       }, 0);
 
-      if (this.wQu === this.allQuizzes[this.wQ].quiz.length - this.lr) {
+      if (this.wQu === this.quizObj.quiz.length - this.lr) {
         this.gameOver = true;
       } else {
         this.noSelectedAnswer = true;
@@ -115,9 +106,10 @@ export default {
       if (this.wQu < 0) {
         this.wQu = 0;
       }
-      this.scoreCard.points.pop();
+      this.scoreCard.answers.points.pop();
     },
     saveAnswer(value) {
+      console.log(value)
       this.noSelectedAnswer = false;
       this.currentAnswer = value;
     },
@@ -128,7 +120,7 @@ export default {
   },
   watch: {
     wQu() {
-      if (this.wQu >= this.allQuizzes[this.wQ].quiz.length - this.lr) {
+      if (this.wQu >= this.quizObj.quiz.length - this.lr) {
         return (this.nextQuestionButtonText = "View Final Score");
       } else {
         return (this.nextQuestionButtonText = ">");
